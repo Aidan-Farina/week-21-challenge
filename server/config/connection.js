b33ca5339ceaad30d2e56e136ba61b9629de2eaf
Aidan <farinaaidan2@gmail.com>
2023-08-22 13:13:59 -0400

    still trying 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;

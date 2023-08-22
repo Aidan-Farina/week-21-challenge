6867606d3869e27d1adf9e2e4f0621aaae2d17e4
Aidan <farinaaidan2@gmail.com>
2023-08-21 23:32:45 -0400

    still trying

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,9 +6,6 @@ const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/g
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  ssl: true,
 });
 
 module.exports = mongoose.connection;

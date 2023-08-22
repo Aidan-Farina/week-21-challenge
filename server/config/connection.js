94a75c84a67bf33c2d05b04d51f09ce9c9d036ff
Aidan <farinaaidan2@gmail.com>
2023-08-21 23:13:44 -0400

    trying to make heroku work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  ssl: true,
+});
 
 module.exports = mongoose.connection;
